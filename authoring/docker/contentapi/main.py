a8d016732f257efe2970fa3128e667439ef920f3
mjoss17 <matthewjoss17@gmail.com>
2022-08-19 09:30:26 -0500

    Move env variables to .env file

--- authoring/docker/contentapi/main.py
+++ authoring/docker/contentapi/main.py
@@ -1,5 +1,4 @@
-from http.client import HTTPException
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List
