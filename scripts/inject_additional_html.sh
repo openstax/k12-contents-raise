84990f274fe76b71f4034973fdbf7949d463ae3b
mjoss17 <matthewjoss17@gmail.com>
2022-08-19 16:42:09 -0500

    Add additional html injection command

---
+++ scripts/inject_additional_html.sh
@@ -0,0 +1,4 @@
+additional_html='<script type="module" crossorigin src="https://k12.openstax.org/apps/raise/index.authoring.8f2edfdb.js"></script>
+<link rel="stylesheet" href="https://k12.openstax.org/apps/raise/index.authoring.5bd2c3c5.css">'
+
+docker-compose exec postgres psql -U moodle -d moodle -c "insert into mdl_config('name', 'value') values('additionalhtmlhead', '$additional_html') on conflict('additionalhtmlhead') do update set value
