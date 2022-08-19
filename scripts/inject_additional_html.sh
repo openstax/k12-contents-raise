7f8d0daa6af2869560dbdcb1716b2ebfbdc9d03f
mjoss17 <matthewjoss17@gmail.com>
2022-08-19 16:48:04 -0500

    Add headers to script

--- scripts/inject_additional_html.sh
+++ scripts/inject_additional_html.sh
@@ -1,4 +1,6 @@
-additional_html='<script type="module" crossorigin src="https://k12.openstax.org/apps/raise/index.authoring.8f2edfdb.js"></script>
+#!/usr/bin/env bash
+
+ADDITONAL_HTML='<script type="module" crossorigin src="https://k12.openstax.org/apps/raise/index.authoring.8f2edfdb.js"></script>
 <link rel="stylesheet" href="https://k12.openstax.org/apps/raise/index.authoring.5bd2c3c5.css">'
 
-docker-compose exec postgres psql -U moodle -d moodle -c "insert into mdl_config('name', 'value') values('additionalhtmlhead', '$additional_html') on conflict('additionalhtmlhead') do update set value=$additional_html
+docker-compose exec postgres psql -U moodle -d moodle -c "insert into mdl_config('name', 'value') values('additionalhtmlhead', '$ADDITONAL_HTML') on conflict('additionalhtmlhead') do update set value=$ADDITONAL_HTML
