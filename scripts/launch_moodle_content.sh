52c539c45fb759b7903376a577d0c534792c0e79
Prabhdip Gill <pg53@rice.edu>
2022-08-24 13:04:46 -0700

    Fix launch command and warn user when selecting destroy

--- scripts/launch_moodle_content.sh
+++ scripts/launch_moodle_content.sh
@@ -1,25 +1,36 @@
 #!/usr/bin/env bash
 
-#up command $1
-if [ $1 == "up" ]; then
+
+ACTION=$1
+
+if [ -z "$ACTION" ]; then
+    echo "Provide one of the following arguments [up, down, destroy]"
+    exit 1
+fi
+
+if [ $ACTION == "up" ]; then
     echo "Starting moodle environment"
+
     docker-compose -f ../authoring/docker/docker-compose.yml up -d
     docker-compose  -f ../authoring/docker/docker-compose.yml exec moodle php admin/cli/install_database.php --agree-license --fullname="Local Dev" --shortname="Local Dev" --summary="Local Dev" --adminpass="admin" --adminemail="admin@acmeinc.com"
-    exec  ../authoring/scripts/inject_additional_html.sh
-
+    bash  ./../authoring/scripts/inject_additional_html.sh ../authoring/docker/docker-compose.yml
     exit 1
 fi
 
-if [ $1 == "down" ]; then
+if [ $ACTION == "down" ]; then
     echo "Stoping moodle environment"
-    docker-compose -f ../authoring/docker/docker-compose.yml down
 
+    docker-compose -f ../authoring/docker/docker-compose.yml down
     exit 1
 fi
 
-if [ $1 == "destroy" ]; then
+if [ $ACTION == "destroy" ]; then
     echo "Destroying moodle environment and state."
-    docker-compose -f ../authoring/docker/docker-compose.yml down -v
 
+    read -p "Are you sure? All database state will be lost. Continue (y/n)? " -n 1 -r
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        docker-compose -f ../authoring/docker/docker-compose.yml down -v
+    fi
     exit 1
 fi
