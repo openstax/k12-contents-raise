97b3f00e7e8472098d64ad2b78f9f31748897471
Prabhdip Gill <pg53@rice.edu>
2022-08-30 08:07:06 -0700

    Change docker-compose to docker compose in helper script

--- authoring/scripts/inject_additional_html.sh
+++ authoring/scripts/inject_additional_html.sh
@@ -5,6 +5,5 @@ set -e
 ADDITONAL_HTML='<script type="module" crossorigin src="https://k12.openstax.org/apps/raise/index.authoring.56ae0462.js"></script>
 <link rel="stylesheet" href="https://k12.openstax.org/apps/raise/index.authoring.5bd2c3c5.css">'
 
-docker-compose exec postgres psql -U moodle -d moodle -c "insert into mdl_config (name, value) values('additionalhtmlhead', '$ADDITONAL_HTML') on conflict(name) do update set value='$ADDITONAL_HTML'"
-
-docker-compose exec moodle php admin/cli/purge_caches.php
+docker compose exec postgres psql -U moodle -d moodle -c "insert into mdl_config (name, value) values('additionalhtmlhead', '$ADDITONAL_HTML') on conflict(name) do update set value='$ADDITONAL_HTML'"
+docker compose exec moodle php admin/cli/purge_caches.php
