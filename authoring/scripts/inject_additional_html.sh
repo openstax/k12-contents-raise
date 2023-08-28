64cfbc73d23af0d3f48b529aa724ae925fe1f12e
Prabhdip Gill <pg53@rice.edu>
2023-08-28 09:30:48 -0700

    Update code versions

--- authoring/scripts/inject_additional_html.sh
+++ authoring/scripts/inject_additional_html.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-ADDITONAL_HTML='<script type="module" crossorigin src="https://k12.openstax.org/apps/raise/index.authoring.8b3e82bb.js"></script>
+ADDITONAL_HTML='<script type="module" crossorigin src="https://k12.openstax.org/apps/raise/index.authoring.f955214f.js"></script>
 <link rel="stylesheet" href="https://k12.openstax.org/apps/raise/index.authoring.f032b861.css">'
 
 docker compose exec postgres psql -U moodle -d moodle -c "insert into mdl_config (name, value) values('additionalhtmlhead', '$ADDITONAL_HTML') on conflict(name) do update set value='$ADDITONAL_HTML'"
