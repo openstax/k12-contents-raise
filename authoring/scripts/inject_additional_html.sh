c90d5c91576d4fcb189cf38c1b050e95baa2b854
Michael Reyna <MReyna12@live.com>
2023-12-12 10:49:14 -0600

    Update auth env code versions

--- authoring/scripts/inject_additional_html.sh
+++ authoring/scripts/inject_additional_html.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-ADDITONAL_HTML='<script type="module" crossorigin src="https://k12.openstax.org/apps/raise/index.authoring.3f5a7033.js"></script>
-<link rel="stylesheet" href="https://k12.openstax.org/apps/raise/index.authoring.551dcb7d.css">'
+ADDITONAL_HTML='<script type="module" crossorigin src="https://k12.openstax.org/apps/raise/index.authoring.77e94fab.js"></script>
+<link rel="stylesheet" href="https://k12.openstax.org/apps/raise/index.authoring.72503d52.css">'
 
 docker compose exec postgres psql -U moodle -d moodle -c "insert into mdl_config (name, value) values('additionalhtmlhead', '$ADDITONAL_HTML') on conflict(name) do update set value='$ADDITONAL_HTML'"
 docker compose exec moodle php admin/cli/purge_caches.php
