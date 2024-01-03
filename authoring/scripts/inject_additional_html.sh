2d62db1a3f475dfbe039904abe80d829af034fc2
Prabhdip Gill <pg53@rice.edu>
2024-01-03 15:44:01 -0800

    Update atrifacts with accordion build

--- authoring/scripts/inject_additional_html.sh
+++ authoring/scripts/inject_additional_html.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-ADDITONAL_HTML='<script type="module" crossorigin src="https://k12.openstax.org/apps/raise/index.authoring.965befd1.js"></script>
-<link rel="stylesheet" href="https://k12.openstax.org/apps/raise/index.authoring.731d8b59.css">'
+ADDITONAL_HTML='<script type="module" crossorigin src="https://k12.openstax.org/apps/raise/index.authoring.d2d0f90a.js"></script>
+<link rel="stylesheet" href="https://k12.openstax.org/apps/raise/index.authoring.70131799.css">'
 
 docker compose exec postgres psql -U moodle -d moodle -c "insert into mdl_config (name, value) values('additionalhtmlhead', '$ADDITONAL_HTML') on conflict(name) do update set value='$ADDITONAL_HTML'"
 docker compose exec moodle php admin/cli/purge_caches.php
