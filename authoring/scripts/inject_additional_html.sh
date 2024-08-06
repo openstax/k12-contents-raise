040bba07c69f3849cb7a5f54e4607360820ef0b3
Prabhdip Gill <pg53@rice.edu>
2024-08-06 09:15:22 -0700

    Update artifacts

--- authoring/scripts/inject_additional_html.sh
+++ authoring/scripts/inject_additional_html.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-ADDITONAL_HTML='<script type="module" crossorigin src="https://k12.openstax.org/apps/raise/index.authoring.v2lzvpZ_.js"></script>
-<link rel="stylesheet" href="https://k12.openstax.org/apps/raise/index.authoring.CAHeIye1.css">'
+ADDITONAL_HTML='<script type="module" crossorigin src="https://k12.openstax.org/apps/raise/index.authoring.CTUvlmor.js"></script>
+<link rel="stylesheet" href="https://k12.openstax.org/apps/raise/index.authoring.Bg8OvSyp.css">'
 
 docker compose exec postgres psql -U moodle -d moodle -c "insert into mdl_config (name, value) values('additionalhtmlhead', '$ADDITONAL_HTML') on conflict(name) do update set value='$ADDITONAL_HTML'"
 docker compose exec moodle php admin/cli/purge_caches.php
