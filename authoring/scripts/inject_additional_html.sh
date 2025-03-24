c6f51566b1db7d88b6c41f40d51e4b9d3827837a
Prabhdip Gill <43251544+P-Gill97@users.noreply.github.com>
2025-03-24 00:15:03 -0700

    Update inject_additional_html.sh

--- authoring/scripts/inject_additional_html.sh
+++ authoring/scripts/inject_additional_html.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 set -e
+
 ADDITONAL_HTML='<script type="module" crossorigin src="https://k12.openstax.org/apps/raise/index.authoring.DEmUcmVP.js"></script>
 <link rel="stylesheet" href="https://k12.openstax.org/apps/raise/index.authoring.C-se0_9E.css">'
 
